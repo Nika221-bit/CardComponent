42ff6e759177502f1ffc2d5a1ba5c2b0b3233d4b
niko5443 <184934855+Nika221-bit@users.noreply.github.com>
2026-07-01 16:57:12 +0400

    done

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,12 +3,13 @@ import image from './assets/react.png'
 function Card(){
    return(
       <div className='Card'>
-           <img src={image} alt='photo of react'></img>
+           <img className='react' src={image} alt='photo of react'></img>
            <h1>this Card is made by a react</h1>
            <h5>and by amazing developer</h5>
       </div>
-   );
 
+   );
+``  
 }
 
 export default Card
