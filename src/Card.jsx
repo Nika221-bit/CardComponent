554775c9629c93adad050960ca431689d24cbd60
niko5443 <184934855+Nika221-bit@users.noreply.github.com>
2026-07-01 14:04:11 +0400

    done(need css)

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+import image from './assets/react.png'
+
+function Card(){
+   return(
+      <div className='Card'>
+           <img src={image} alt='photo of react'></img>
+           <h1>this Card is made by a react</h1>
+           <h5>and by amazing developer</h5>
+      </div>
+   );
+
+}
+
+export default Card
